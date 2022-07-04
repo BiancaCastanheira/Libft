b641585678e54e85b9d8845d8d6e0b6992138953
BiancaCastanheira <castanheira.bi@gmail.com>
2022-07-04 20:37:05 -0300

    ✨ ft_strlen.c

---
+++ ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biacast5 <biacast5@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 20:31:55 by biacast5          #+#    #+#             */
+/*   Updated: 2022/07/04 20:36:40 by biacast5         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	ft_strlen(const char *s)
+{
+	int	lenght;
+
+	lenght = 0;
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char s[] = "42 - the meaning of life, the universe, and everything";
+
+// 	printf("The lengh of your input is: %zu \n", ft_strlen(s));
+// }
