2047cae1c83afc94b71e285869d54059967c9def
BiancaCastanheira <castanheira.bi@gmail.com>
2022-07-16 23:43:35 -0300

    ✨ ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biacast5 <biacast5@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 23:41:45 by biacast5          #+#    #+#             */
+/*   Updated: 2022/07/16 23:43:08 by biacast5         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*dest2;
+	char	*src2;
+	size_t	i;
+
+	dest2 = (char *)dest;
+	src2 = (char *)src;
+	i = 0;
+	while (i < n)
+	{
+		if (src < dest)
+		{
+			dest2[n - i - 1] = src2 [n - i - 1];
+		}
+		else
+		{
+			dest2[i] = src2[i];
+		}
+		i++;
+	}
+	return (dest);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char srt1test[] = "BLACKHOLE";
+// 	char srt2test[] = "SURVIVER!";
+// 	char str1[] = "Blackhole";
+// 	char str2[] = "Surviver!";
+
+// 	printf("Before ft_memmove: %s \n", str1);
+
+// 	ft_memmove(str1, str2, 3);
+// 	printf("Result after ft_memmove: %s \n", str1);
+
+// 	memmove(srt1test, srt2test, 3);
+// 	printf("Function default value: %s \n", srt1test);
+// }
